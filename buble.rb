3572d26be4a3260dc387f3f6bf155c0ae50f780d
Adrian <adfpizarro@gmail.com>
2020-06-12 13:02:23 -0600

    Added bubble sort by method

--- buble.rb
+++ buble.rb
@@ -11,3 +11,23 @@ def bubble_sort (arr)
   end
   p arr
 end
+
+def bubble_sort_by (arr)
+  changes=true
+  while changes do
+    changes=false
+    (arr.length-1).times do |i|
+      if yield(arr[i],arr[i+1])>0
+        arr[i],arr[i+1]=arr[i+1],arr[i]
+        changes=true
+      end
+    end
+  end
+  p arr
+end
+
+
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
